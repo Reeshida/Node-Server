ea70eba5b1817171af25d137b88ccdf848e1fac7
Reeshida <reeshida@hotmail.com>
2018-07-30 19:31:48 -0400

    3rd time is the charm

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const http = require('http');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT ||8080;
 
 function handleRequest(request, response) {
     const path = request.url;
-response.end("Heroku");
-   
+
+    response.end("hello friend");
+    
+    // switch (path) {
+    //     case "/home":
+    //         displayRoot(request, response);
+    //         break;
+    //     default:
+    //         response.writeHead(404);
+    //         response.end();
+    //         break;
+    // }
+};
+
+// function displayRoot(req, res) {
+//     const html = `
+//     <html>
+//         <body>
+//             <h1>Learn Heroku</h1>
+//         </body>
+//     </html>
+//     `;
+//     res.end(html);
+// }
+
 const server = http.createServer(handleRequest);
 
 server.listen(port, function() {
     console.log(`Server listening on port ${port}`);
-})
-}
+});
